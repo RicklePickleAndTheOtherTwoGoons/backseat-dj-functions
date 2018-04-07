f9a8c418ace5df2e6ecfc545c70974dadc5ab855
Chandler Willoughby <chandler243@gmail.com>
2018-04-06 23:48:42 -0400

    Create new session code

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+function makeid() {
+	var text = "";
+	var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for (var i=0;i<4;i++) {
+		text += chars.charAt(Math.floor(Math.random()*chars.length));
+	}
+	return text
+}
+
+exports.createNewSession = functions.https.onRequest((req,res) => {
+	const sessionCode = makeid();
+	return admin.database().ref('/queues').push({sessionCode: sessionCode}).then((snapshot) => {
+		return res.redirect(303, snapshot.ref.toString());
+	})
+})
